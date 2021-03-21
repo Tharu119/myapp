482f97f0d8897640aa1b929304f77215b1c66eb6
Tharu#119 <tharukamadhushanka9558@gmail.com>
2021-03-21 13:16:33 +0530

    OneClick Mad Assignment 01

---
+++ app/src/main/java/com/example/oneclick/Payment.java
@@ -0,0 +1,13 @@
+package com.example.oneclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Payment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
